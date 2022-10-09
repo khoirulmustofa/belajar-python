2440a75bb0bc4dc4f23a2fc505f89dde368e4060
Khoirul Mustofa <khoirul.mustofa2014@gmail.com>
2022-10-09 14:26:25 +0700

    Operator, Logical, Comparasion
    Operator, Logical, Comparasion

---
+++ Operator.py
@@ -0,0 +1,16 @@
+
+
+
+a = int(input("Masukan Bilangan Satu :"))
+b = int(input("Masukan Bilangan Dua :"))
+
+# Arithmetic Operators 
+
+print(f"a + b = {a + b}")
+print(f"a - b = {a - b}")
+print(f"a * b = {a * b}")
+print(f"a / b = {a / b}")
+# modulus adalah sisa hasil bagi %
+print(f"a % b = {a % b}") 
+print(f"a ** b = {a ** b}")
+print(f"a // b = {a // b}")
