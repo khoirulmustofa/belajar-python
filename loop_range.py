c308a15419525c004fd1dc1dd08a54358c29278b
Khoirul Mustofa <khoirul.mustofa2014@gmail.com>
2023-02-04 14:26:31 +0700

    UP

--- loop_range.py
+++ loop_range.py
@@ -26,3 +26,40 @@ print(squares)
 
 # Output:
 # [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+
+for i in range(3):
+    for j in range(3):
+        print("i:", i, "j:", j)
+
+# Output:
+# i: 0 j: 0
+# i: 0 j: 1
+# i: 0 j: 2
+# i: 1 j: 0
+# i: 1 j: 1
+# i: 1 j: 2
+# i: 2 j: 0
+# i: 2 j: 1
+# i: 2 j: 2
+
+
+# for i in range(1, 101):
+#     # kalo bilangan itu ganjil maka cetak tulisan GANJIL
+#     # kalo bilangan itu genap maka cetak tulisan GENAP
+#     print(i)
+
+for number in range(1, 101):
+    if number % 2 == 0:
+        print(f"{number} GENAP")
+    else:
+        print(f"{number} GANJIL")
+
+# 1 GANJIL
+# 2 GENAP
+
+
+# Habis di bagi 3 cetak BOOM
+# Habis dibagi 5 Cetak DOOR
+# Kalo habis dibagi 3 dan 5 cetak JOSS
+
---
+++ tugas_loop.py
@@ -0,0 +1,9 @@
+# bilangan 1 sampai 100
+# Habis di bagi 3 cetak BOOM
+# Habis dibagi 5 Cetak DOOR
+# Kalo habis dibagi 3 dan 5 cetak JOSS
+
+print("Hallo")
+# for number range(1,101)
+#     if number % 3 == 0:
+#         print(f"{number} GENAP")
